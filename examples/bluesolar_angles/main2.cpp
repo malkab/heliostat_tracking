4eaa8bb6a791bf09960d41e8efa810b04ea53eb0
mblancomuriel <manuel.jesus.blanco@gmail.com>
2024-09-30 02:25:16 +0200

    Modification of bluesolar_angles to allow for calibration.

--- examples/bluesolar_angles/main2.cpp
+++ examples/bluesolar_angles/main2.cpp
@@ -1,5 +1,9 @@
 #include <iomanip>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <cmath>
 
 #include "TrackerArmature2A.h"
 #include "TrackerTarget.h"
@@ -8,6 +12,161 @@
 #include "ElevationAngleKM.h"
 #include "HourAngleKM.h"
 #include "BluesolarHelpers.h"
+
+// Function to compare floating point values with a tolerance
+bool CompareDoubles(double a, double b, double tolerance = 1e-6) {
+    return std::abs(a - b) < tolerance;
+}
+
+// Function to check if the heliostat location matches any in the calibrated set
+std::tuple<bool, int> IdentifyHeliostatByLocation(double x, double y, double z, const std::map<int, std::tuple<double, double, double>>& calibratedHeliostats) {
+    for (const auto& entry : calibratedHeliostats) {
+        const auto& [calX, calY, calZ] = entry.second;
+        if (CompareDoubles(x, calX) && CompareDoubles(y, calY) && CompareDoubles(z, calZ)) {
+            return {true, entry.first}; // Return true and the corresponding heliostatID
+        }
+    }
+    return {false, -1}; // No match found
+}
+
+struct CalibrationPoint {
+    double azimuth;        // Azimuth in degrees
+    double elevation;      // Elevation in degrees
+    double correctionElevation; // Correction for the elevation actuator
+    double correctionHour;      // Correction for the hour actuator
+};
+
+struct CalibrationDataScattered {
+    std::vector<CalibrationPoint> points; // List of scattered calibration points
+};
+
+// Hardcoded map for calibrated heliostats identified by heliostatID
+std::map<int, std::tuple<double, double, double>> calibratedHeliostatLocations = {
+    {78, {3.05075, 42.68150, 1.64000}},
+    {79, {-0.80225, 42.69550, 1.49000}},
+    {81, {-4.63725, 42.71550, 1.37000}},
+    {89, {3.02575, 46.91250, 1.66000}},
+    {90, {-0.79425, 46.91350, 1.66000}},
+    {92, {-4.59025, 46.91950, 1.53000}},
+    {100, {3.03675, 51.15050, 1.84000}},
+    {101, {-0.79125, 51.16450, 1.78000}},
+    {103, {-4.59325, 51.11450, 1.63000}}                       
+    // Add more calibrated heliostats here
+};
+
+std::map<int, CalibrationDataScattered> heliostatCorrections = {
+    {78, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {79, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {81, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {89, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {90, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {92, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {100, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {101, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }},
+    {103, {
+        {   // List of scattered calibration points for heliostat 78
+            { { -90.0, 10.0, 0.001, 0.004 },  // {azimuth, elevation, correctionElevation, correctionHour}
+              { -45.0, 20.0, 0.0015, 0.0045 },
+              { 0.0, 30.0, 0.002, 0.005 },
+              { 45.0, 40.0, 0.0015, 0.0045 },
+              { 90.0, 50.0, 0.001, 0.004 } }
+        }
+    }}
+    // Add more corrections here for other heliostats
+};
+
+CalibrationPoint WeightedInterpolation(
+    const CalibrationDataScattered& data, 
+    double targetAzimuth, double targetElevation)
+{
+    double weightSum = 0.0;
+    double weightedCorrectionElevation = 0.0;
+    double weightedCorrectionHour = 0.0;
+
+    for (const auto& point : data.points) {
+        double distance = sqrt(pow(point.azimuth - targetAzimuth, 2) + pow(point.elevation - targetElevation, 2));
+        if (distance == 0.0) {
+            // If the point matches exactly, return its correction
+            return point;
+        }
+        double weight = 1.0 / distance;  // Inverse distance weighting
+        weightedCorrectionElevation += weight * point.correctionElevation;
+        weightedCorrectionHour += weight * point.correctionHour;
+        weightSum += weight;
+    }
+
+    CalibrationPoint result;
+    result.azimuth = targetAzimuth;
+    result.elevation = targetElevation;
+    result.correctionElevation = weightedCorrectionElevation / weightSum;
+    result.correctionHour = weightedCorrectionHour / weightSum;
+
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
@@ -53,6 +212,20 @@ int main(int argc, char *argv[])
 
     double length_elevation_angle_actuator = elevation_angle_km.getActuatorLengthFromElevationAngle(elevation_angle);
     double length_hour_angle_actuator = hour_angle_km.getActuatorLengthFromHourAngle(hour_angle);
+
+    // Identify the heliostat by its location
+    auto [isCalibrated, heliostatID] = IdentifyHeliostatByLocation(xHeliostat, yHeliostat, zHeliostat, calibratedHeliostatLocations);
+
+    if (isCalibrated && heliostatCorrections.find(heliostatID) != heliostatCorrections.end()) {
+        const CalibrationDataScattered& data = heliostatCorrections[heliostatID];
+
+        // Interpolate corrections using scattered data based on sun azimuth and elevation
+        CalibrationPoint correction = WeightedInterpolation(data, sunAzimuth, sunElevation);
+
+        // Apply the corrections to the actuator lengths
+        length_elevation_angle_actuator += correction.correctionElevation;
+        length_hour_angle_actuator += correction.correctionHour;
+    }
 
     // Printing the results
     std::cout << target->angles << ", " << length_elevation_angle_actuator << ", " << length_hour_angle_actuator << std::endl;
